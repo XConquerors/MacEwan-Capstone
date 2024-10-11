1eb5000539b9916e5a1667ac078730b2ec6ff1c8
sharmac7 <sharmac7@mymacewan.ca>
2024-10-10 18:11:47 -0600

    add priority controller

---
+++ capstone.web.api/Controllers/PrioritiesController.cs
@@ -0,0 +1,42 @@
+﻿using capstone.web.api.Data;
+using capstone.web.api.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace capstone.web.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PrioritiesController : ControllerBase
+    {
+
+        private readonly AppDbContext _context;
+
+        public PrioritiesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Priorities
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Priority>>> GetCategories()
+        {
+            return await _context.Priorities.Where(a => !a.IsDeleted).ToListAsync();
+        }
+
+        // GET: api/Priorities/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Priority>> GetCategory(int id)
+        {
+            var priority = await _context.Priorities.FindAsync(id);
+
+            if (priority == null)
+            {
+                NotFound();
+            }
+
+            return priority;
+        }
+    }
+}
