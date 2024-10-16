ff4243016a4a25af0e2ac4ced192c0d90116d804
Parvinder <kaurp349@mymacewan.ca>
2024-10-15 18:14:44 -0600

    Done App routing and solve erros in all components

--- capstone.web/src/app/app-routing.module.ts
+++ capstone.web/src/app/app-routing.module.ts
@@ -6,15 +6,25 @@ import { HeaderComponent } from './header/header.component';
 import { AuthGuard } from './core/security/auth.guard';
 import { CategoryComponent } from './components/category/category.component';
 import { CategoryListComponent } from './components/category-list/category-list.component';
+import { PriorityComponent } from './components/priority/priority.component';
+import { PriorityListComponent } from './components/priority-list/priority-list.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: '', component: HeaderComponent, canActivate: [AuthGuard] }, // Example default page
-  { path: "categories", component : CategoryListComponent},
-  { path: "category/:id", component: CategoryComponent },
-  { path: "category", component: CategoryComponent},
-  { path: "", redirectTo: "/", pathMatch: "full" },
-  { path: '**', redirectTo: '' },
+  {
+    path: '',
+    component: HeaderComponent,
+    canActivate: [AuthGuard],
+    children: [
+      { path: 'categories', component: CategoryListComponent },
+      { path: 'category/:id', component: CategoryComponent },
+      { path: 'category', component: CategoryComponent },
+      { path: 'priorities', component: PriorityListComponent },
+      { path: 'priority/:id', component: PriorityComponent },
+      { path: 'priority', component: PriorityComponent },
+    ]
+  },
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
