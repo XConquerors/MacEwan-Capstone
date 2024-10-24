3236e16f909c8775dcd1bb10d5dd206906ae5fb9
Harikrishnan Venugopal <venugopalh@mymacewan.ca>
2024-10-24 17:38:44 -0600

    todo controllers and seeded data

---
+++ capstone.web.api/Controllers/TodoController.cs
@@ -0,0 +1,41 @@
+﻿using capstone.web.api.Data;
+using capstone.web.api.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace capstone.web.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDoController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ToDoController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ToDo
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ToDo>>> GetToDos()
+        {
+            return await _context.ToDos.ToListAsync();
+        }
+
+        // GET: api/ToDo/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ToDo>> GetToDo(int id)
+        {
+            var toDo = await _context.ToDos.FindAsync(id);
+
+            if (toDo == null)
+            {
+                return NotFound();
+            }
+
+            return toDo;
+        }
+    }
+}
