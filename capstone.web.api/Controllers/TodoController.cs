11c557b986af1b7de29c7ad3636b67a118ec96d1
simmakm <simmakm@mymacewan.ca>
2024-11-05 17:13:03 -0700

    search and add todo controller

--- capstone.web.api/Controllers/TodoController.cs
+++ capstone.web.api/Controllers/TodoController.cs
@@ -75,5 +75,56 @@ namespace capstone.web.api.Controllers
             
             return CreatedAtAction(nameof(GetToDo), new { id = newToDo.ToDoId }, newToDo);
         }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteToDo(int id)
+        {
+            var todo = await _context.ToDos.FindAsync(id);
+            if (todo == null)
+            {
+                NotFound();
+            }
+            todo.IsDeleted = true;
+            //_context.ToDo.Remove(todo);
+            _context.SaveChanges();
+            return NoContent();
+        }
+        // PUT: api/ToDos/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateToDo(int id, ToDo todo)
+        {
+            if (id != todo.ToDoId)
+            {
+                return BadRequest("ToDo ID mismatch.");
+            }
+            var existingToDo = await _context.ToDos.FindAsync(id);
+            if (existingToDo == null)
+            {
+                return NotFound();
+            }
+            existingToDo.Name = todo.Name;
+            existingToDo.Description = todo.Description;
+            existingToDo.IsDeleted = todo.IsDeleted;
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ToDoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return NoContent();
+        }
+        private bool ToDoExists(int id)
+        {
+            return _context.ToDos.Any(e => e.ToDoId == id);
+        }
     }
 }
